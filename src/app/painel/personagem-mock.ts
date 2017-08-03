72f1388be43498b8810cbdd3f60d9baf95e39d99
Leonardo Dionizio <leodionizio13@gmail.com>
2017-08-03 11:10:27 -0300

    adicionado novos personagens na class-mock

--- src/app/painel/personagem-mock.ts
+++ src/app/painel/personagem-mock.ts
@@ -4,11 +4,26 @@ export const PERSONAGENS: Personagem[] = [
   {
     urlFotoPersonagem: './assets/personagens/daenerys-targaryen.jpg',
     nomePersonagem: 'daenerys targaryen',
-    dicaPersonagem: 'Eu sou a mãe de dragões!'
+    dicaPersonagem: 'Eu sou a mãe de dragões e quebradora de correntes!'
   },
   {
     urlFotoPersonagem: './assets/personagens/jon-snow.jpg',
     nomePersonagem: 'jon snow',
     dicaPersonagem: 'Eu sou o rei do norte e não sei de nada!'
+  },
+  {
+    urlFotoPersonagem: './assets/personagens/tyrion-lannister.jpg',
+    nomePersonagem: 'tyrion lannister',
+    dicaPersonagem: 'Eu sou um bastardo aos olhos de meu pai!'
+  },
+  {
+    urlFotoPersonagem: './assets/personagens/joffrey-baratheon.jpg',
+    nomePersonagem: 'joffrey baratheon',
+    dicaPersonagem: 'Eu sou um bastardo aos olhos dos 7 reinos e todos me odeiam!'
+  },
+  {
+    urlFotoPersonagem: './assets/personagens/ned-stark.jpg',
+    nomePersonagem: 'ned stark',
+    dicaPersonagem: 'Eu sou o protetor do norte, todos me amam, mas costumo perder a cabeça!'
   }
 ];
